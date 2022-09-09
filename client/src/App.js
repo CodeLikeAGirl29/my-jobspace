39cff9e35362c026559737b982dc52052c699bbe
Lindsey Howard <miss_leading009@yahoo.com>
2022-09-09 06:07:26 -0400

    fixed default route

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Register, Landing, Error, ProtectedRoute } from './pages'
+import { Landing, Register, Error, ProtectedRoute } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
-  AllJobs,
-  Profile,
-  SharedLayout,
-  Stats,
-  AddJob,
-} from './pages/dashboard'
+	Profile,
+	AllJob,
+	AddJob,
+	SharedLayout,
+	Stats,
+} from "./pages/dashboard";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <ProtectedRoute>
-              <SharedLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Stats />} />
-          <Route path='all-jobs' element={<AllJobs />} />
-          <Route path='add-job' element={<AddJob />} />
-          <Route path='profile' element={<Profile />} />
-        </Route>
-        <Route path='/register' element={<Register />} />
-        <Route path='/landing' element={<Landing />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<ProtectedRoute>
+							<SharedLayout />
+						</ProtectedRoute>
+					}
+				>
+					<Route index path='stats' element={<Stats />} />
+					<Route path='all-jobs' element={<AllJob />} />
+					<Route path='add-job' element={<AddJob />} />
+					<Route path='profile' element={<Profile />} />
+				</Route>
+				<Route path='/register' element={<Register />} />
+				<Route path='/landing' element={<Landing />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
