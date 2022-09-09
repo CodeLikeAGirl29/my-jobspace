2239515048cf13332135ea9346d4805081c78a7e
Lindsey Howard <miss_leading009@yahoo.com>
2022-09-09 12:06:09 -0400

    some changes

--- server.js
+++ server.js
@@ -5,14 +5,22 @@ dotenv.config();
 import "express-async-errors";
 import morgan from "morgan";
 
-//db and authenticateUser
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
+// db and authenticateUser
 import connectDB from "./db/connect.js";
 
-//routes
-import authRoutes from "./routes/authRoutes.js";
-import jobsRoutes from "./routes/jobsRoutes.js";
+// routes
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
-//middleware
+// middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import authenticateUser from "./middleware/auth.js";
@@ -21,14 +29,23 @@ if (process.env.NODE_ENV !== "production") {
 	app.use(morgan("dev"));
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// when ready to deploy
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
-app.get("/api/v1", (req, res) => {
-	res.json({ msg: "API" });
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/jobs", authenticateUser, jobsRoutes);
+// when ready to deploy
+ app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+ })
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -37,8 +54,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
 	try {
-		const conn = await connectDB(process.env.MONGO_URL);
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
+		await connectDB(process.env.MONGO_URL);
 		app.listen(port, () => {
 			console.log(`Server is listening on port ${port}...`);
 		});
